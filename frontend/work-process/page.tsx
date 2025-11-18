18d6cee6afb0b8e8b451ebffa1d92ebabadaf099
abhishek737383 <karanmehata1654@gmail.com>
2025-11-18 12:34:37 +0530

    Build successful

---
+++ frontend/work-process/page.tsx
@@ -0,0 +1,21 @@
+// Correct example
+export default function WorkProcess() {
+  return (
+    <div>
+      <h1>Work Process Page</h1>
+      {/* Your page content */}
+    </div>
+  );
+}
+
+// OR
+const WorkProcess = () => {
+  return (
+    <div>
+      <h1>Work Process Page</h1>
+      {/* Your page content */}
+    </div>
+  );
+};
+
+export default WorkProcess;
