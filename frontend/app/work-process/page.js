0f1ec6e4e6c30f49ae38831e6f931291ea96014b
abhishek737383 <karanmehata1654@gmail.com>
2025-11-18 12:43:03 +0530

    Build successful

---
+++ frontend/app/work-process/page.js
@@ -0,0 +1,4 @@
+// app/work-process/page.jsx
+export default function Page() {
+  return <main><h1>Work process</h1></main>;
+}
--- frontend/work-process/page.js
+++
@@ -1,4 +0,0 @@
-// app/work-process/page.jsx
-export default function Page() {
-  return <main><h1>Work process</h1></main>;
-}
